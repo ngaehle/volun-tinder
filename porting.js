eb5813010ec5f9bfbbb53075ead8b273cae1cf8d
Nicole Gaehle <nmgb79@mail.umsl.edu>
2019-02-16 18:06:43 -0600

    Update porting.js

--- porting.js
+++ porting.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
     var url = "orgs.json";
-    $.ajax({
-        dataType: "json",
-        url: url,
-        data: data,
-        success: function(data) {
-            console.log(data);
-        }
+    // $.ajax({
+    //     dataType: "json",
+    //     url: url,
+    //     data: data,
+    //     success: function(data) {
+    //         console.log(data);
+    //     }
+    // });
+    $.getJSON(url, function(data) {
+        var items = [];
+        $.each(data, function(key, val) {
+            items.push("<li id='" + key + "'>" + val + "</li>");
+        });
+
+        $("<ul/>", {
+            "class": "my-new-list",
+            html: items.join("")
+        }).appendTo("body");
     });
 });
